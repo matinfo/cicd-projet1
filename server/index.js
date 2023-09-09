80a62bf61aff86c35e216ced12244eb01459bc82
dymafr <contact@dyma.fr>
2023-09-09 18:11:33 +0200

    first commit

---
+++ server/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const port = process.env.PORT || '3000';
+const router = require('./routes');
+
+require('./database');
+
+const app = express();
+app.use(express.static('../client/dist'));
+app.use(express.json());
+app.use(cookieParser());
+
+app.use(router);
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+});
+
+app.listen(port);
