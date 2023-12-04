1363d5d25366ba51ceddfd2c2e7cdc5761c6be3f
Quentin <quentindevial19@gmail.com>
2023-12-04 18:00:10 +0100

    fix: dependencies

--- server/database/index.js
+++ server/database/index.js
@@ -1,20 +1,19 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const { MongoMemoryServer } = require("mongodb-memory-server");
 
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === "production") {
   mongoose
     .connect(
-      'mongodb+srv://jean:123@cluster0.urpjt.gcp.mongodb.net/vue3c23?retryWrites=true&w=majority'
+      "mongodb+srv://jean:123@cluster0.urpjt.gcp.mongodb.net/vue3c23?retryWrites=true&w=majority"
     )
     .then(() => {
-      console.log('Connected [DB PROD] !');
+      console.log("Connected [DB PROD] !");
     })
     .catch((e) => console.log(e));
 } else {
-  const { MongoMemoryServer } = require('mongodb-memory-server');
-
   MongoMemoryServer.create().then(async (mongo) => {
     const uri = mongo.getUri();
     await mongoose.connect(uri);
-    console.log('Connected [DB test] !');
+    console.log("Connected [DB test] !");
   });
 }
