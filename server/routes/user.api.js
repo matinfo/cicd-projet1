6758667978a9238d530d6f947d547e380f123c31
Dyma <32801166+dymafr@users.noreply.github.com>
2024-06-22 16:55:55 +0200

    Update user.api.js

--- server/routes/user.api.js
+++ server/routes/user.api.js
@@ -1,10 +1,10 @@
-const router = require('express').Router();
-const UserModel = require('../database/models/user.model');
-const bcrypt = require('bcrypt');
-const jsonwebtoken = require('jsonwebtoken');
-const { keyPub } = require('../env/keys');
+const router = require("express").Router();
+const UserModel = require("../database/models/user.model");
+const bcrypt = require("bcrypt");
+const jsonwebtoken = require("jsonwebtoken");
+const { keyPub } = require("../env/keys");
 
-router.post('/', async (req, res) => {
+router.post("/", async (req, res) => {
   const body = req.body;
   const user = new UserModel({
     name: body.name,
@@ -15,17 +15,18 @@ router.post('/', async (req, res) => {
     await user.save();
     res.json(null);
   } catch (err) {
+    console.error(err);
     res.status(400).json("Erreur lors de l'inscription");
   }
 });
 
-router.get('/current', async (req, res) => {
+router.get("/current", async (req, res) => {
   const token = req.cookies?.token;
   if (token) {
     try {
       const decodedToken = jsonwebtoken.verify(token, keyPub);
       const user = await UserModel.findById(decodedToken.sub)
-        .select('-password -__v')
+        .select("-password -__v")
         .exec();
       if (user) {
         res.json(user);
@@ -33,11 +34,17 @@ router.get('/current', async (req, res) => {
         res.json(null);
       }
     } catch (e) {
+      console.error(e);
       res.json(null);
     }
   } else {
     res.json(null);
   }
+});
+
+router.post("/login", async (req, res) => {
+  // Pour tester le Test coverage vizualizer
+  return res.json({ message: "test" });
 });
 
 module.exports = router;
