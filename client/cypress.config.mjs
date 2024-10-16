17acf5efe98fa72fe9aecaae48b0ebdd9711e595
Mathieu Meylan <m.meylan@depsys.com>
2024-10-16 15:13:00 +0200

    fix cypress config

--- client/cypress.config.mjs
+++ client/cypress.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'cypress'
+import cypressCodeCoverageTask from '@cypress/code-coverage/task'
 
 export default defineConfig({
   e2e: {
     specPattern: 'cypress/e2e/**/*.{cy,spec}.{js,jsx,ts,tsx}',
     baseUrl: 'http://localhost:4173',
-    setupNodeEvents(on: any, config: any) {
-      require('@cypress/code-coverage/task')(on, config);
+    setupNodeEvents(on, config) {
+      cypressCodeCoverageTask(on, config);
       return config;
     }
   }
